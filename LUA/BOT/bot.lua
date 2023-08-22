cd2d08e57bbdcfe0370800264f570443d73e4dcb
Lee-projecthelios <eve.projecthelios@gmail.com>
2023-08-23 00:31:05 +0100

    LuaDiscordiaBot

---
+++ LUA/BOT/bot.lua
@@ -0,0 +1,14 @@
+local discordia = require('discordia')
+local client = discordia.Client()
+
+client:on('ready', function()
+	print('Logged in as '.. client.user.username)
+end)
+
+client:on('messageCreate', function(message)
+	if message.content == '!ping' then
+		message.channel:send('Pong!')
+	end
+end)
+
+client:run('bot MTE0MzY4NjQxMDE4MDc2Nzg3Ng.GLDyc5.Y5mhXB6nqEFAm21KbmLmH3uAuqpfoLccCZfd5U')
